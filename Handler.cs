7a21deb9b953303fb861ac51ab9d243cfc013db2
Ingrid <ingridsandev@gmail.com>
2019-12-30 00:12:56 +0000

    Removing sln

--- Handler.cs
+++ Handler.cs
@@ -1,4 +1,7 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Newtonsoft.Json;
 using System;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -7,10 +10,25 @@ namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public Response S3ThumbnailGeneratorAsync(Request request)
-       {
-           return new Response("Go Serverless v1.0! Your function executed successfully!", request);
-       }
+        IAmazonS3 S3Client { get; set; }
+
+        public Handler()
+        {
+            S3Client = new AmazonS3Client();
+        }
+
+        public Handler(IAmazonS3 s3Client)
+        {
+            S3Client = s3Client;
+        }
+
+        public Response S3ThumbnailGeneratorAsync(S3Event evnt, ILambdaContext context)
+        {
+            Console.WriteLine("Event: " + JsonConvert.SerializeObject(evnt));
+            Console.WriteLine("Context: " + JsonConvert.SerializeObject(context));
+
+            return new Response("Ingrid");
+        }
     }
 
     public class Response
@@ -18,9 +36,8 @@ namespace AwsDotnetCsharp
       public string Message {get; set;}
       public Request Request {get; set;}
 
-      public Response(string message, Request request){
+      public Response(string message){
         Message = message;
-        Request = request;
       }
     }
 
