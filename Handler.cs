580a7e91c3d3acaafcc925b6aa8ea5364705986d
Ingrid <ingridsandev@gmail.com>
2019-12-31 01:01:17 +0000

    Adding function content to resize images when uploading to a S3 bucket

--- Handler.cs
+++ Handler.cs
@@ -1,16 +1,26 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Amazon.S3;
-using Newtonsoft.Json;
+using Amazon.S3.Model;
 using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using Amazon.S3.Util;
+using System.Collections.Generic;
 
-[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-        IAmazonS3 S3Client { get; set; }
+        private IAmazonS3 S3Client { get; set; }
+        private readonly string[] _supportedImageTypes = new string[] { ".png", ".jpg", ".jpeg" };
 
         public Handler()
         {
@@ -22,35 +32,84 @@ namespace AwsDotnetCsharp
             S3Client = s3Client;
         }
 
-        public Response S3ThumbnailGeneratorAsync(S3Event evnt, ILambdaContext context)
+        public async Task S3ThumbnailGeneratorAsync(S3Event evnt, ILambdaContext context)
         {
-            Console.WriteLine("Event: " + JsonConvert.SerializeObject(evnt));
-            Console.WriteLine("Context: " + JsonConvert.SerializeObject(context));
+            try
+            {
+                foreach (var record in evnt.Records)
+                {
+                    // Validate extention
+                    if (!_supportedImageTypes.Contains(Path.GetExtension(record.S3.Object.Key).ToLower()))
+                    {
+                        Console.WriteLine("File extention is not supported - " + record.S3.Object.Key);
+                        continue;
+                    }
 
-            return new Response("Ingrid");
+                    // Validating if image has already been compressed
+                    if (await CheckCompressedTagAsync(record))
+                    {
+                        Console.WriteLine($"Image {record.S3.Bucket.Name}:{record.S3.Object.Key} has already been compressed");
+                        continue;
+                    }
+
+                    // Resize the image according to the size set in the enviroment variables
+                    var imageStream = await ResizeImageAsync(record);
+
+                    // Put in the same S3 bucket the image resized ending in "_thumbnail"
+                    await PutResizedObjectAsync(record, imageStream);
+                }
+
+                Console.WriteLine("Thumbnail has been created");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error getting object {evnt.Records?[0].S3.Object.Key} from bucket {evnt.Records?[0].S3.Bucket.Name}. Make sure they exist and your bucket is in the same region as this function.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
         }
-    }
 
-    public class Response
-    {
-      public string Message {get; set;}
-      public Request Request {get; set;}
+        private async Task<bool> CheckCompressedTagAsync(S3EventNotification.S3EventNotificationRecord record) =>
+            (await S3Client.GetObjectTaggingAsync(new GetObjectTaggingRequest
+            {
+                BucketName = record.S3.Bucket.Name,
+                Key = record.S3.Object.Key
+            })).Tagging.Any(tag => tag.Key == "Compressed" && tag.Value == "true");
 
-      public Response(string message){
-        Message = message;
-      }
-    }
+        private async Task PutResizedObjectAsync(S3EventNotification.S3EventNotificationRecord record, Stream imageStream) =>
+            await S3Client.PutObjectAsync(new PutObjectRequest
+            {
+                BucketName = record.S3.Bucket.Name,
+                Key = $"{Path.GetFileNameWithoutExtension(record.S3.Object.Key)}_thumbnail{Path.GetExtension(record.S3.Object.Key)}",
+                InputStream = imageStream,
+                TagSet = new List<Tag>
+                {
+                    new Tag
+                    {
+                        Key = "Compressed",
+                        Value = "true"
+                    }
+                }
+            });
 
-    public class Request
-    {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
+        private async Task<Stream> ResizeImageAsync(S3EventNotification.S3EventNotificationRecord record)
+        {
+            var imageSize = int.Parse(Environment.GetEnvironmentVariable("THUMBNAIL_SIZE"));
 
-      public Request(string key1, string key2, string key3){
-        Key1 = key1;
-        Key2 = key2;
-        Key3 = key3;
-      }
+            Stream imageStream = new MemoryStream();
+            using (var objectResponse = await S3Client.GetObjectAsync(record.S3.Bucket.Name, record.S3.Object.Key))
+            using (Stream responseStream = objectResponse.ResponseStream)
+            {
+                using (Image<Rgba32> image = Image.Load(responseStream))
+                {
+                    image.Mutate(ctx => ctx.Resize(imageSize, imageSize));
+                    image.Save(imageStream, new JpegEncoder());
+                    imageStream.Seek(0, SeekOrigin.Begin);
+                }
+            }
+
+            return imageStream;
+        }
     }
 }
--- deploy.cmd
+++ deploy.cmd
@@ -1,2 +1,3 @@
 dotnet restore
 dotnet lambda package --configuration release --framework netcoreapp2.1 --output-package bin/release/netcoreapp2.1/S3ThumbnailGenerator.zip
+sls deploy -u
